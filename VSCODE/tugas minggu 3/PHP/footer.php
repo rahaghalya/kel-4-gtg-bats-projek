41de0ed589ec8726e5fe7879eac7852a55962da6
FadilPrasetyo <padilprasetyo63@gmail.com>
2026-02-09 14:50:23 +0700

    Menghapus file yang tidak diperlukan

--- VSCODE/tugas minggu 3/PHP/footer.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<p>Copyright &copy: 2013-" . date("Y") . " www.aji-firman.net</p>";
-?>
--- VSCODE/tugas minggu 3/PHP/mainprogram2.php
+++
@@ -1,5 +0,0 @@
-<h1>Welcome to my home page!</h1>
-<?php 
-include 'variabel.php';
-echo "I have a $color $car.";
-?>
